d4e8f2e723a94a72072525d140fb5f33f83b28ca
אסף אלבז <asafelbaz1@gmail.com>
2025-04-21 15:55:04 +0300

    Added functions picture[1-7]() for printing the states of the hangman

---
+++ Hangman/PrintStates.py
@@ -0,0 +1,59 @@
+def picture1():
+    print("x-------x\n")
+def picture2():
+    print("""
+    x-------x
+    |
+    |
+    |
+    |
+    |
+    """)
+def picture3():
+    print("""
+    x-------x
+    |       |
+    |       0
+    |
+    |
+    |
+    """)
+
+def picture4():
+    print("""
+    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |
+    """)
+def picture5():
+    print(r"""
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |
+    |
+
+    """)
+def picture6():
+    print(r"""
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      /
+    |
+    """)
+
+def picture7():
+    print(r"""
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      / \
+    |
+    """)
