21e3c480eb42e066c29f7ccd1f36fedee8f37231
אסף אלבז <asafelbaz1@gmail.com>
2025-04-29 15:02:23 +0300

    Completed Unit 3! (for real this time)

--- Hangman/HangmanGame.py
+++ Hangman/HangmanGame.py
@@ -13,5 +13,10 @@ def prompt_player_for_guess():
     players_guess = input("Guess a letter\n")
     print (players_guess.lower())
 
+def print_game_board():
+    word_to_guess = input("Write a word (no spaces)\n")
+    print('_ ' * len(word_to_guess))
+
 print(HANGMAN_ASCII_ART, "\n",MAX_TRIES)
+print_game_board()
 prompt_player_for_guess()
